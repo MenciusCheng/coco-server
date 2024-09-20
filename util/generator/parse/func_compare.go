cf76693657fd4293fec17a902ebb2b2d59eb0bb0
chengmengwei <295567172@qq.com>
2024-09-20 15:48:50 +0800

    增加函数

--- util/generator/parse/func_compare.go
+++ util/generator/parse/func_compare.go
@@ -4,6 +4,7 @@ import "fmt"
 
 func init() {
 	funcMap["eqStr"] = FuncCompareObj.EqStr
+	funcMap["inStr"] = FuncCompareObj.InStr
 }
 
 type FuncCompare struct{}
@@ -12,4 +13,13 @@ var FuncCompareObj = &FuncCompare{}
 
 func (c *FuncCompare) EqStr(s1, s2 interface{}) bool {
 	return fmt.Sprintf("%v", s1) == fmt.Sprintf("%v", s2)
+}
+
+func (c *FuncCompare) InStr(s1 interface{}, arr ...interface{}) bool {
+	for _, s2 := range arr {
+		if fmt.Sprintf("%v", s1) == fmt.Sprintf("%v", s2) {
+			return true
+		}
+	}
+	return false
 }
