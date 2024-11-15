9269c561760378747c42c2b7ab50b111e2742fcc
chengmengwei <295567172@qq.com>
2024-11-15 15:56:43 +0800

    移除旧书签

--- util/bookmark/model/folder_tree.go
+++
@@ -1,8 +0,0 @@
-package model
-
-type FolderTree struct {
-	ID         uint          `json:"id"`
-	Name       string        `json:"name"`
-	Bookmarks  []*Bookmark   `json:"bookmarks"`
-	SubFolders []*FolderTree `json:"sub_folders"`
-}
